90e328451299fc09734f25c6fbc382cad33d5609
sam <sam.bogu@gmail.com>
2025-03-05 17:46:17 +0100

    Add structure supporting separate commands per location

--- main.py
+++ main.py
@@ -8,14 +8,32 @@ You can exit the game with 'exit'.
 """
 
 locations = {
-  "entrance": "You are in the entrance area. Behind you is a broken elevator. In front of you a leather couch. The hallway continues to the [east].",
-  "hallway1": "You are in a hallway with a table. The table has a bowl of apples on it. Maybe you can [take] one? The way returns [west]."
+  "entrance": {
+    "look": {
+      "output": "You are in the entrance area. Behind you is a broken elevator. In front of you a leather couch. The hallway continues to the [east].",
+      "next_location_id": None
+    },
+    "east": {
+      "output": "You go east.",
+      "next_location_id": "hallway1"
+    },
+  },
+  "hallway1": {
+    "look": {
+      "output": "You are in a hallway with a table. The table has a bowl of apples on it. Maybe you can [take] one? The way returns [west].",
+      "next_location_id": None
+    },
+    "west": {
+      "output": "You go west.",
+      "next_location_id": "entrance"
+    },
+  }
 }
 
 current_location = locations["entrance"]
 
 print(welcome_message)
-print(current_location)
+print(current_location["look"]["output"])
 
 while True:
   user_input = input("> ")
@@ -23,16 +41,15 @@ while True:
   if user_input == "exit":
     print("Thanks for playing!")
     break
-  elif user_input == "look":
-    print(current_location)
-  elif current_location == locations["entrance"] and user_input == "east":
-    current_location = locations["hallway1"]
-    print(current_location)
-  
-  elif current_location == locations["hallway1"] and user_input == "west":
-    current_location = locations["entrance"]
-    print(current_location)
-  
+  if user_input in current_location:
+    output = current_location[user_input]["output"]
+    next_location_id = current_location[user_input]["next_location_id"]
+    
+    print(output)
+
+    if next_location_id:
+      current_location = locations[next_location_id]
+      print(current_location["look"]["output"])
+    
   else:
     print("Please enter a valid command.")
-
