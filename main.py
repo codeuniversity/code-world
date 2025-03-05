564202d929fb7a3ce5e419b0747f8c834aaa042f
sam <sam.bogu@gmail.com>
2025-03-05 17:44:01 +0100

    Add a second room

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ You can exit the game with 'exit'.
 ~~~~~~~~~~~~~~~~~~~~~
 """
 
-current_location = "You are in the entrance area. Behind you is a broken elevator. In front of you a leather couch. The hallway continues to the [east]."
+locations = {
+  "entrance": "You are in the entrance area. Behind you is a broken elevator. In front of you a leather couch. The hallway continues to the [east].",
+  "hallway1": "You are in a hallway with a table. The table has a bowl of apples on it. Maybe you can [take] one? The way returns [west]."
+}
+
+current_location = locations["entrance"]
 
 print(welcome_message)
 print(current_location)
@@ -20,6 +25,14 @@ while True:
     break
   elif user_input == "look":
     print(current_location)
+  elif current_location == locations["entrance"] and user_input == "east":
+    current_location = locations["hallway1"]
+    print(current_location)
+  
+  elif current_location == locations["hallway1"] and user_input == "west":
+    current_location = locations["entrance"]
+    print(current_location)
+  
   else:
     print("Please enter a valid command.")
 
